6a46ae773ce16266d41753425c5672da014743a1
Yuri Perin <y.perin@scalablesolutions.io>
2024-08-16 21:23:40 +0700

    small fix

---
+++ react_frontend/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+  const location = useLocation();
+  const isAuthenticated = localStorage.getItem('access_token') !== null;
+
+  if (!isAuthenticated) {
+    // Redirect them to the /signin page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/signin" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
